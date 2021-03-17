1642da132522c48057fff4181cbc68b7d2b5e6f7
zhengzhichao <377950622@qq.com>
2021-03-17 14:49:48 +0800

    乱码处理

--- node/stream/index.js
+++ node/stream/index.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const readable = fs.createReadStream('./read.txt');
 const writeable = fs.createWriteStream('./write.txt');
 
+// 如果是中文
+readable.setEncoding('utf8');
+
 // readable.pipe(writeable);
 
 // end 读取结束时终止写入流，默认值是 true
