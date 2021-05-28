d43c07c31b3f15fd603d4ac40bc5be81e6f03e7d
zhengzhichao <377950622@qq.com>
2021-05-28 11:48:40 +0800

    sql练习

--- mysql/test_code.sql
+++ mysql/test_code.sql
@@ -5,7 +5,7 @@ SELECT name FROM test GROUP BY name HAVING min(grade) >= 80;
 
 -- 2
 DELETE FROM test WHERE id NOT IN (
-	SELECT MIN(id) as id FROM 
+	SELECT id FROM 
 	(SELECT * FROM test) as table1
 	GROUP BY table1.name, table1.course, table1.grade
 );
@@ -13,7 +13,7 @@ DELETE FROM test WHERE id NOT IN (
 -- 3
 SELECT a.name as one, b.name as two
 FROM test3 a, test3 b
-WHERE a.name < b.name
+WHERE a.name < b.name;
 
 
 -- 4
@@ -25,7 +25,7 @@ FROM test4 a,
 			WHERE name='a'
 			GROUP BY month
 		) b
-WHERE a.month = b.month AND a.money > b.money
+WHERE a.month = b.month AND a.money > b.money;
 
 -- 5
 SELECT 
@@ -41,7 +41,7 @@ SELECT
 	(SELECT num_id FROM test6 m WHERE s_id=1 AND m.p_id = test6.p_id) as s1_num,
 	(SELECT num_id FROM test6 m WHERE s_id=2 AND m.p_id = test6.p_id) as s2_num
 FROM test6
-GROUP BY p_id
+GROUP BY p_id;
 
 -- 正题
 -- 1
@@ -49,13 +49,113 @@ SELECT s.*, a.score as score_01, b.score as score_02
 FROM student s,
      (SELECT sid, score FROM sc WHERE cid=01) a,
      (SELECT sid, score FROM sc WHERE cid=02) b
-WHERE a.sid = b.sid AND a.score > b.score AND s.sid = a.sid
+WHERE a.sid = b.sid AND a.score > b.score AND s.sid = a.sid;
 
 -- 2
-SELECT min(s.sid) as sid, min(sname) as sname, avg(score) as avg_score
+SELECT sid, sname, avg(score) as avg_score
 FROM student as s, sc
 WHERE s.sid = sc.sid
 GROUP BY s.sid
-HAVING avg_score > 60
+HAVING avg_score > 60;
 
 -- 3
+SELECT s.* 
+FROM student s
+WHERE sid in (SELECT sid FROM sc WHERE score IS NOT NULL);
+
+-- 4
+SELECT s.sid as '学号', s.sname as '学生名', COUNT(cid) as '选课总数', sum(score) as '总成绩'
+FROM student as s LEFT JOIN sc
+ON s.sid = sc.sid
+GROUP BY s.sid;
+
+-- 4.1
+select s.sid, s.sname, count(*) as 选课总数, sum(score) as 总成绩,
+    sum(case when cid = 01 then score else null end) as score_01,
+    sum(case when cid = 02 then score else null end) as score_02,
+    sum(case when cid = 03 then score else null end) as score_03
+from student as s, sc
+where s.sid = sc.sid
+group by s.sid;
+
+-- 5
+SELECT COUNT(id) as t_num FROM teacher 
+WHERE tname like '李%';
+
+-- 6
+SELECT s.*
+FROM student s
+		 INNER JOIN
+		 sc on s.sid = sc.sid
+		 INNER JOIN
+		 course c on sc.cid = c.cid
+		 INNER JOIN
+		 teacher t on c.tid = t.tid
+WHERE t.tname='张三';
+
+-- 7
+select * from student where sid in (select sid from sc group by sid having count(cid) < 3);
+
+-- 9
+SELECT s.*
+FROM student s
+WHERE s.sid in (
+    select sid from sc
+    where cid in (select cid from sc where sid = '01') and sid <>'01'
+    group by sid
+    having COUNT(cid)>=3				
+	);
+
+-- 8
+select * from student where sid in(
+    select distinct sid from sc where cid in(
+        select cid from sc where sid='01'
+    ) and sid <> '01'
+);
+
+-- 10
+SELECT sname FROM student
+WHERE sid not in (
+	SELECT sc.sid 
+	FROM sc
+			 INNER JOIN
+			 course c on sc.cid = c.cid
+			 INNER JOIN
+			 teacher t on c.tid = t.tid
+	 WHERE t.tname = '张三'
+);
+
+-- 11
+select s.sid, s.sname, avg(score)
+from student as s, sc
+where s.sid = sc.sid and score<60
+group by s.sid
+having count(score)>=2;
+
+-- 12
+select s.*, sc.score
+from student as s, sc
+where s.sid = sc.sid and sc.score<60
+ORDER BY sc.score desc;
+
+-- 13
+select sid,
+    sum(case when cid=01 then score else null end) as score_01,
+    sum(case when cid=02 then score else null end) as score_02,
+    sum(case when cid=03 then score else null end) as score_03
+from sc group by sid
+order by avg(score) desc;
+
+-- 14
+select c.cid as 课程号, c.cname as 课程名称, count(*) as 选修人数,
+    max(score) as 最高分, min(score) as 最低分, avg(score) as 平均分,
+    sum(case when score >= 60 then 1 else 0 end)/count(*) as 及格率,
+    sum(case when score >= 70 and score < 80 then 1 else 0 end)/count(*) as 中等率,
+    sum(case when score >= 80 and score < 90 then 1 else 0 end)/count(*) as 优良率,
+    sum(case when score >= 90 then 1 else 0 end)/count(*) as 优秀率
+from sc, course c
+where c.cid = sc.cid
+group by c.cid
+order by count(*) desc, c.cid asc;
+
+-- 15
