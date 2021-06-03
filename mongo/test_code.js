cec0e49cb23c104c771848e47f7f52eaf10a49a4
zhengzhichao <377950622@qq.com>
2021-06-03 18:02:48 +0800

    mongodb-test

--- mongo/test_code.js
+++ mongo/test_code.js
@@ -34,4 +34,33 @@ db.users.find({age: { $lt: 25 }});
 // 6
 db.users.find({age: { $gte: 25, $lte: 25 }});
 
+// 7
+db.users.find({name: { $regex : /bu/ }});
+
+// 8
+db.users.find({name: { $regex : /^ju/ }});
+db.users.find({name: { $regex : /bu$/ }});
+
+// 9
+db.users.find({},{_id: 0, name:1, age:1});
+
+// 10
+db.users.find({},{"comments":0});
+
+// 11
+db.users.find({ age: {$gt:25}},{ age:1, name:1});
+
+// 12
+db.users.find().sort({age:-1});
+
+// 13
+db.users.find().limit(3);
+
+// 14
+db.users.find().skip(3);
+
+// 15
+db.users.find().skip(3).limit(3);
+
+
 
