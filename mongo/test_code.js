7a8ef923cbb1931367c288519d1abab3d25cc3ef
zhengzhichao <377950622@qq.com>
2021-06-04 13:56:01 +0800

    mongo-test

--- mongo/test_code.js
+++ mongo/test_code.js
@@ -51,6 +51,7 @@ db.users.find({},{"comments":0});
 db.users.find({ age: {$gt:25}},{ age:1, name:1});
 
 // 12
+db.users.find().sort({age:1});
 db.users.find().sort({age:-1});
 
 // 13
@@ -61,6 +62,90 @@ db.users.find().skip(3);
 
 // 15
 db.users.find().skip(3).limit(3);
+
+// 16
+db.users.find(
+	{
+		age:{
+			$in: [25,26]
+		}
+	},
+	{
+		_id:0,
+		name: 1,
+		age:1	
+	}
+);
+db.users.find(
+	{
+		$or:[
+			{
+				age: 25
+			},
+			{
+				age: 26
+			}
+		]
+	},
+	{
+		_id:0,
+		name: 1,
+		age:1	
+	}
+);
+
+// 18
+db.users.find({comments:{$exists:true}});
+db.users.aggregate([
+	{ 
+		$project: { 
+			_id: 0,
+			name: 1,
+			rname:"$name",
+			comments: 1
+		},
+	},
+	{
+		$match: {
+		  comments: {
+				$nin: [null]
+			}
+		},
+	}	
+]);
+
+// 20
+db.users.find(
+{ 
+	name: { $regex : /bu/ },
+	age: { $in: [25, 26] }
+}
+);
+
+// 21
+db.users.distinct('age');
+
+// 22
+db.users.find({name: 'xiaobu'},{'comments':{$slice: [0, 3]}});
+
+// 23
+db.users.find({name: 'xiaobu'},{'comments':{$slice: [3, 3]}, $id :1});
+
+// 24
+db.users.find({comments:{'userId': '101', 'content': '评论101', 'commentTime' : '2018-04-22 22:50:48.39'}});
+
+// 25
+db.users.find({"comments.userId":{$in:["001", "101"]}});
+
+// 26
+db.users.find({"comments":{$elemMatch:{"userId": "101", "content": "评论101"}}});
+
+// 27
+var tom = db.users.findOne({_id :ObjectId( "60b88dfb30053217d852c882")});
+var dbref = tom.comments;
+db[dbref.$ref].findOne({_id :dbref.$id});
+
+
 
 
 
